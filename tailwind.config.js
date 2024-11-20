b2611f70e022120239e36da1573b8527c421d108
Sevara <nematullayevasevara6@gamil.com>
2024-11-20 12:42:15 +0500

    homework with issues in responsiveness

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'], // Update with the paths to your files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+  
+
